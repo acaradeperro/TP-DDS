8b0f6760ee49aaef19bc6f2e1889dd8a2bc76175
lucascharlab <lucascharlab@gmail.com>
2017-05-31 01:28:37 -0300

    Subo de nuevo bd

--- Dump20170531.sql
+++ Dump20170531.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `bd` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `bd`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: bd
@@ -131,4 +133,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-31  1:21:46
+-- Dump completed on 2017-05-31  1:27:24
